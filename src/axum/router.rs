14482e5158810e0083197bcbee472932ad04d757
Haf <haf@protonmail.ch>
2024-04-21 19:30:04 +0200

    full path to inner macro

--- src/axum/router.rs
+++ src/axum/router.rs
@@ -3,7 +3,7 @@ macro_rules! __route {
     ($router:ident,) => {};
     ($router:ident, $method:ident $path:expr => $handler:expr; $($tail:tt)*) => {
         $router = $router.route($path, axum::routing::casey::lower!($method)($handler));
-        __route!($router, $($tail)*)
+        $crate::__route!($router, $($tail)*)
     };
 }
 
@@ -11,12 +11,12 @@ macro_rules! __route {
 #[macro_export]
 macro_rules! router {
     ($router:ident, { $($tail:tt)* } ) => {
-        __route!(router, $($tail)*);
+        $crate::__route!(router, $($tail)*);
     };
     ($($tail:tt)*) => {
         {
             let mut router = axum::Router::new();
-            __route!(router, $($tail)*);
+            $crate::__route!(router, $($tail)*);
             router
         }
     };
